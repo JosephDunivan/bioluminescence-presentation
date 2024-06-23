bc5155e3c7adcca2e608c5717154e47423d1d472
Joseph Dunivan <josephdunivanucf@gmail.com>
2024-06-23 13:24:12 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Illuminating Our Waters`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
